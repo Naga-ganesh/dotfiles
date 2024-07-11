76f7f20d55b61b2ca82f288dd815ebc07805f090
nagaganeshwar <naga.ganeshwar@chronus.com>
2024-07-11 15:36:13 +0530

    add nvim-python config

---
+++ nvim-python/.config/nvim/lua/keymaps.lua
@@ -0,0 +1,19 @@
+-- space bar leader key
+vim.g.mapleader = " "
+
+-- buffers
+vim.keymap.set("n", "<leader>n", ":bn<cr>")
+vim.keymap.set("n", "<leader>p", ":bp<cr>")
+vim.keymap.set("n", "<leader>x", ":bd<cr>;:bp<cr>")
+
+-- save the file
+vim.keymap.set("n", "<leader>w", ":w<cr>")
+-- save and exit the file
+vim.keymap.set("n", "<leader>wq", ":wq<cr>")
+
+-- jump to normal mode
+vim.keymap.set("i", "jk", "<Esc>")
+
+-- black python formatting
+vim.keymap.set("n", "<leader>fmp", ":silent !black %<cr>")
+
