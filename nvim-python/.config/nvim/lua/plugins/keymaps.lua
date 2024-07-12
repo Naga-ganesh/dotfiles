b8912fa92f16bd9a9f7a7f1eb65fefaf7dc9b533
nagaganeshwar <naga.ganeshwar@chronus.com>
2024-07-12 13:02:00 +0530

    add gitsigns keymaps

--- nvim-python/.config/nvim/lua/plugins/keymaps.lua
+++ nvim-python/.config/nvim/lua/plugins/keymaps.lua
@@ -19,3 +19,6 @@ vim.keymap.set({"n", "v"}, "<leader>c", ":CommentToggle<cr>")
 -- Git keymaps
 vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<cr>")
 vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<cr>")
+vim.keymap.set("n", "<leader>gu", ":Gitsigns undo_stage_hunk<cr>")
+vim.keymap.set("n", "<leader>gr", ":Gitsigns reset_hunk<cr>")
+vim.keymap.set("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<cr>")
