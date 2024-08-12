53cf85edffd4db0b1cc96f1fc6dca03b49efbec7
Naga-ganesh <nagaganesh3108@gmail.com>
2024-08-13 00:15:34 +0530

    configure gh & gl to go to the first and last word in a line in my nvim-python setup

--- nvim-python/.config/nvim/lua/keymaps.lua
+++ nvim-python/.config/nvim/lua/keymaps.lua
@@ -13,6 +13,8 @@ vim.keymap.set("n", "<leader>wq", ":wq<cr>")
 
 -- jump to normal mode
 vim.keymap.set("i", "jk", "<Esc>")
+vim.keymap.set("n", "gl", "$")
+vim.keymap.set("n", "gh", "0")
 
 -- black python formatting
 vim.keymap.set("n", "<leader>fmp", ":silent !black %<cr>")
